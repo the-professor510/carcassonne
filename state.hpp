3f3749e3f1d43ce8772a548c60261328a34fcd2e
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2021-05-26 13:04:38 +0100

    planning comments

--- state.hpp
+++ state.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
 class State {
-
+    // things to have!
+    //      - disjoint-set data structure for tracking connected zones.
+    //      - "head" node for the board, that we can add to and like, move in from
+    //      - perhaps keep track of a "border" to make move generation simpler
 };
